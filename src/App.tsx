7ba191b2d467b71242bf4e9e9dca04d20c129a66
rrrrho <rocioghillino@gmail.com>
2025-10-18 20:52:46 -0300

    uploaded app + pipeline yml + kubernetes config

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>CD / CI</h1>
+
+      <p className="read-the-docs">
+        Equipo 10 - Ingenieria de Software
+      </p>
+    </>
+  )
+}
+
+export default App
