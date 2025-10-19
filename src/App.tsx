bf897ec5062b79dda02ac4ae85f963f92ba210a8
rrrrho <rocioghillino@gmail.com>
2025-10-18 22:11:57 -0300

    added dog

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import reactLogo from './assets/react.svg'
+import perro from './assets/perro.png'
 import './App.css'
 
 function App() {
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a target="_blank">
+          <img src={perro} className="logo" alt="React logo" />
         </a>
       </div>
       <h1>CD / CI</h1>
 
-      <p className="read-the-docs">
+      <p className="subtitle-home">
         Equipo 10 - Ingenieria de Software
       </p>
     </>
