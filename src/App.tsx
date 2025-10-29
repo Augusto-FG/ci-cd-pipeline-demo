71bc7aee37e3bb7182f8562b66a56480dd8803a9
Augusto Fernández <augufernandez@uade.edu.ar>
2025-10-29 02:08:16 -0300

    update title

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
       <p className="subtitle-home">
         Equipo 10 - Ingenieria de Software
-        UADE
+        Universidad argentina de la empresa
       </p>
     </>
   )
